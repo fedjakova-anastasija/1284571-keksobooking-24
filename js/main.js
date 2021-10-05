02c1d47a3ff530a78ac9d877e30c21d33a1f186c
Федякова Анастасия <fedyakova.anastasia@gmail.com>
2021-10-05 23:09:52 +0300

    Добавила функцию вывода рандомного числа

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
-function getRandomNumber(min, max, precision) {
-  if ((min || max || precision) < 0) {
-    console.error('Диапазон и точность могут быть только положительными, включая ноль.');
-    return;
+const getRandomNumber = (min, max) => {
+  const precision = 4;
+
+  if (min < 0 || max < 0) {
+    throw ('Диапазон и точность могут быть только положительными, включая ноль.');
   }
 
   if (min > max) {
-    console.error('Значение «до» должно должно быть больше, чем значение «от».');
-    return;
+    throw ('Значение «до» должно должно быть больше, чем значение «от».');
   }
 
   return ((Math.random() * (max - min)) + min).toPrecision(precision + 1);
-}
+};
 
-getRandomNumber(3, 5, 0);
-getRandomNumber(5, 5, 1);
-getRandomNumber(0, 5, 2);
-getRandomNumber(5, 3, 3);
-getRandomNumber(-3, 5, 4);
-getRandomNumber(3, -5, 5);
-getRandomNumber(-3, -5, 6);
+getRandomNumber(3, 5);
+getRandomNumber(3, 5);
+getRandomNumber(5, 5);
+getRandomNumber(0, 5);
+getRandomNumber(5, 3);
+getRandomNumber(-3, 5);
+getRandomNumber(3, -5);
+getRandomNumber(-3, -5);
