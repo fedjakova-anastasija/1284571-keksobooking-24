0915ed2a051929239a9c8c31817c3b58bd6a31b8
Федякова Анастасия <fedyakova.anastasia@gmail.com>
2021-10-18 00:37:25 +0300

    Отрисовала отображение объявления

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import {createNotice} from './data.js';
+import {showOffer} from './popup.js';
 
 const SIMILAR_NOTICE_COUNT = 10;
 
 const similarNotices = Array.from({length: SIMILAR_NOTICE_COUNT}, createNotice);
 
-similarNotices;
+showOffer(similarNotices);
---
+++ js/popup.js
@@ -0,0 +1,68 @@
+const TYPES_NAME = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalow': 'Бунгало',
+  'hotel': 'Отель',
+};
+
+const offers = document.querySelector('#map-canvas');
+const similarListFragment = document.createDocumentFragment();
+const similarOfferTemplate = document.querySelector('#card').content.querySelector('.popup');
+
+const showPhotos = (offer, offerElement) => {
+  const photosListFragment = document.createDocumentFragment();
+  const photosContainer = offerElement.querySelector('.popup__photos');
+  const photoTemplate = photosContainer.querySelector('.popup__photo');
+
+  offer.photos.forEach(photoUrl => {
+    const photoElement = photoTemplate.cloneNode(true);
+    photoElement.setAttribute('src', photoUrl);
+    photosListFragment.appendChild(photoElement);
+  });
+
+  photosContainer.innerHTML = '';
+  photosContainer.append(photosListFragment);
+};
+
+const showFeatures = (offer, offerElement) => {
+  const features = offer.features;
+
+  const featureContainer = offerElement.querySelector('.popup__features');
+  const featureList = featureContainer.querySelectorAll('.popup__feature');
+  const modifiers = features.map((feature) => 'popup__feature--' + feature);
+
+  featureList.forEach(featureListItem => {
+    const modifier = featureListItem.classList[1];
+
+    if (!modifiers.includes(modifier)) {
+      featureListItem.remove();
+    }
+  });
+};
+
+const showOffer = (similarNotices) => {
+  similarNotices.slice(-1).forEach(({offer, author}) => {
+    const offerElement = similarOfferTemplate.cloneNode(true);
+
+    const title = offerElement.querySelector('.popup__title');
+    offer.title ? title.innerHTML = offer.title : title.classList.add('hidden');
+
+    offerElement.querySelector('.popup__text--address').innerHTML = offer.address;
+    offerElement.querySelector('.popup__text--price').innerHTML = offer.price + ' ₽/ночь';
+    offerElement.querySelector('.popup__type').innerHTML = TYPES_NAME[offer.type];
+    offerElement.querySelector('.popup__text--capacity').innerHTML = offer.rooms + ' комнат для ' + offer.guests + ' гостей';
+    offerElement.querySelector('.popup__text--time').innerHTML = 'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
+    offerElement.querySelector('.popup__description').innerHTML = offer.description;
+    offerElement.querySelector('.popup__avatar').setAttribute('src', author.avatar);
+
+    showFeatures(offer, offerElement);
+    showPhotos(offer, offerElement);
+
+    similarListFragment.appendChild(offerElement);
+  });
+
+  offers.appendChild(similarListFragment);
+};
+
+export {showOffer};
