e0b84e6e22a3d951549dad42cb627ce270c70640
Федякова Анастасия <fedyakova.anastasia@gmail.com>
2021-10-27 20:40:33 +0300

    Добавила валидацию формы

--- js/user-form.js
+++ js/user-form.js
@@ -2,19 +2,13 @@ const userRoomNumberSelect = document.querySelector('#room_number');
 const userCapacitySelect = document.querySelector('#capacity');
 const userSubmitButton = document.querySelector('.ad-form__submit');
 
-userCapacitySelect.addEventListener('change', (evt) => {
-  evt.preventDefault();
-  userCapacitySelect.setCustomValidity('');
-});
-
-userSubmitButton.addEventListener('click', () => {
+const checkCapacity = () => {
   const userCapacityNumberValue = Number(userCapacitySelect.value);
   const userRoomNumberValue = Number(userRoomNumberSelect.value);
 
-  if ( userRoomNumberValue === 100) {
-    if (userCapacityNumberValue !== 0) {
-      userCapacitySelect.setCustomValidity('Необходимо выбрать поле "не для гостей"');
-    }
+  if (userRoomNumberValue === 100 && userCapacityNumberValue !== 0) {
+    userCapacitySelect.setCustomValidity('Необходимо выбрать поле "не для гостей"');
+    userCapacitySelect.reportValidity();
 
     return;
   }
@@ -22,9 +16,21 @@ userSubmitButton.addEventListener('click', () => {
   if (userCapacityNumberValue > userRoomNumberValue && userCapacityNumberValue !== 0) {
     const invalidText = userRoomNumberValue === 1 ? 'поле "для 1 гостя"' : `${userRoomNumberValue} или менее гостей`;
     userCapacitySelect.setCustomValidity(`Необходимо выбрать ${invalidText}`);
-  } else if (userCapacityNumberValue === 0) {
+  } else if (userCapacityNumberValue === 0 && userRoomNumberValue !== 100) {
     userCapacitySelect.setCustomValidity('Необходимо выбрать поле с количеством гостей');
   } else {
     userCapacitySelect.setCustomValidity('');
   }
+  userCapacitySelect.reportValidity();
+};
+
+userCapacitySelect.addEventListener('change', () => {
+  checkCapacity();
+});
+userRoomNumberSelect.addEventListener('change', () => {
+  checkCapacity();
+});
+
+userSubmitButton.addEventListener('click', () => {
+  checkCapacity();
 });
